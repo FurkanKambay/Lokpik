0862d7e56a96f84862fd1d7ad386aeb3072295f5
Furkan Kambay <furkankambay@protonmail.com>
2026-01-01 14:57:03 +0300

    AssetReserializeHelper

---
+++ Assets/Scripts/Editor/AssetReserializeHelper.cs
@@ -0,0 +1,14 @@
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+
+namespace Lokpik.Editor
+{
+    public sealed class AssetReserializeHelper : MonoBehaviour
+    {
+        [MenuItem("Tools/Force Reserialize Assets")]
+        private static void ForceReserializeAssets() =>
+            AssetDatabase.ForceReserializeAssets();
+    }
+}
+#endif
