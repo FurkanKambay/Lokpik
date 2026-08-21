3dd9828379d1043320a69ab2b98e9301b59dc760
Furkan Kambay <furkankambay@protonmail.com>
2026-08-22 02:48:29 +0300

    PickControlsConfig

--- Assets/Scripts/Data/PickControlsConfig.cs
+++ Assets/Scripts/Data/PickControlsConfig.cs
@@ -4,10 +4,44 @@ namespace Lokpik.Data
 {
     public interface IPickControlsConfig
     {
+        float ChamberRetargetRate { get; }
+        bool UseTensionDrift { get; }
+        float TensionDrift { get; }
+        float MinTorque { get; }
+        float MaxTorque { get; }
+        float TurnSpeed { get; }
+        float PlugGravity { get; }
     }
 
     [CreateAssetMenu(menuName = "Pick/Controls Config")]
     internal sealed class PickControlsConfig : ScriptableObject, IPickControlsConfig
     {
+        [Header("Chamber Switching")]
+        [SerializeField, Min(0)] private float chamberRetargetRate = 0.3f;
+        // TODO: dynamic pick position with chamber walls
+
+        [Header("Artificial Skill Hindrance")]
+        [SerializeField] private bool useTensionDrift;
+        [SerializeField, Min(0)] private float tensionDrift = 0.008f;
+
+        [Header("Plug Torque")]
+        [Tooltip("Minimum torque required to turn plug.")]
+        [SerializeField, Range(0, 1)] private float minTorque = 0.4f;
+        [Tooltip("Maximum torque the plug can handle before binding the pin.")]
+        [SerializeField, Range(0, 1)] private float maxTorque = 0.9f;
+
+        [Header("Plug Rotation")]
+        [SerializeField, Min(0)] private float turnSpeed = 1f;
+        [SerializeField, Min(0)] private float plugGravity = 5f;
+
+        public float ChamberRetargetRate => chamberRetargetRate;
+        public bool UseTensionDrift => useTensionDrift;
+        public float TensionDrift => tensionDrift;
+
+        public float MinTorque => minTorque;
+        public float MaxTorque => maxTorque;
+
+        public float TurnSpeed => turnSpeed;
+        public float PlugGravity => plugGravity;
     }
 }
--- Assets/Scripts/Lockpicker.cs
+++ Assets/Scripts/Lockpicker.cs
@@ -8,23 +8,7 @@ namespace Lokpik
     {
         [Header("References")]
         [SerializeField] private TumblerLock tumblerLock;
-
-        [Header("Input Config")]
-        [SerializeField, Min(0)] private float chamberRetargetRate = 0.3f;
-
-        [Header("Artificial Skill Hindrance")]
-        [SerializeField] private bool useTensionDrift;
-        [SerializeField, Min(0)] private float tensionDrift = 0.008f;
-
-        [Header("Pin Setting")]
-        [Tooltip("Minimum torque required to turn plug.")]
-        [SerializeField, Range(0, 1)] private float minTorque = 0.5f;
-        [Tooltip("Maximum torque the plug can handle before binding the pin.")]
-        [SerializeField, Range(0, 1)] private float maxTorque = 0.8f;
-
-        [Header("Plug Rotation")]
-        [SerializeField, Min(0)] private float turnSpeed = 1f;
-        [SerializeField, Min(0)] private float plugGravity = 5f;
+        [SerializeField] private PickControlsConfig controls;
 
         [Header("Debug")]
         [SerializeField, Range(0, 5)] private int targetedPin;
@@ -33,8 +17,8 @@ namespace Lokpik
 
         public TumblerLock Lock => tumblerLock;
         public int TargetedPin => targetedPin;
-        public float MinTorque => minTorque;
-        public float MaxTorque => maxTorque;
+        public float MinTorque => controls ? controls.MinTorque : -1f;
+        public float MaxTorque => controls ? controls.MaxTorque : -1f;
 
         public float AppliedTorque
         {
@@ -68,7 +52,7 @@ namespace Lokpik
         private void HandleChangePin()
         {
             chamberRetargetTimer += Time.deltaTime;
-            if (chamberRetargetTimer < chamberRetargetRate)
+            if (chamberRetargetTimer < controls.ChamberRetargetRate)
                 return;
 
             int delta = input.PickMoveDelta;
@@ -85,23 +69,23 @@ namespace Lokpik
         private void ApplyTorque()
         {
             float tension = input.Tension;
-            if (useTensionDrift && tension > 0)
+            if (controls.UseTensionDrift && tension > 0)
             {
-                float randomDrift = Random.Range(-tensionDrift, tensionDrift);
+                float randomDrift = Random.Range(-controls.TensionDrift, controls.TensionDrift);
                 tension += randomDrift;
             }
 
             AppliedTorque = tension;
 
             // TODO: move this all into TumblerLock?
-            bool lowTorque = appliedTorque < minTorque; // not enough to Set any pin
-            bool highTorque = appliedTorque > maxTorque; // too much for the pin to move
+            bool lowTorque = appliedTorque < controls.MinTorque; // not enough to Set any pin
+            bool highTorque = appliedTorque > controls.MaxTorque; // too much for the pin to move
             int tensionValue = lowTorque ? -1 : highTorque ? 1 : 0;
 
             float turnDelta =
-                highTorque ? turnSpeed
-                : lowTorque ? -plugGravity
-                : turnSpeed;
+                highTorque ? controls.TurnSpeed
+                : lowTorque ? -controls.PlugGravity
+                : controls.TurnSpeed;
 
             tumblerLock.RotatePlug(turnDelta * Time.deltaTime, tensionValue);
         }
